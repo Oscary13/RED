0b6efdbe2232f130535f9efd77ecedd3f9aeadd2
Oscary13 <oscary_1320114020@uptecamac.edu.mx>
2021-11-04 18:31:49 -0600

    OBb

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
 <?php
-echo "HOLA MUNDO desde la web"
+ $nombre ="Oscary";
+ $apellidoP ="Basurto";
+ $apellidoM = "Basurto";
+ $edad = "19";
+ $gustos = ["Jugar Futbol", "Escuchar Rap","Programar","Jugar videojuegos","Aprender cosas nuevas"];
+echo '
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio</title>
+</head>
+<body>
+    <table class="table table-light">
+    <thead class="thead-light">
+        <tr>
+            <th>Nombre</th>
+            <th>Apellido Paterno</th>
+            <th>Apellido Materno</th>
+            <th>Edad</th>
+            <th>Gustos</th>
+            
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>';echo $nombre; echo'</td>
+            <td>';echo $apellidoP; echo'</td>
+            <td>';echo $apellidoM; echo'</td>
+            <td>';echo $edad; echo'</td>
+            <td>';echo json_encode($gustos); echo'</td>
+        </tr>
+    </tbody>
+</table>
+    
+    
+    '; 
+    
+   
+    echo '
+
+</body>
+</html>';
 ?>
